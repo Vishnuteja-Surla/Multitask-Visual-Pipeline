3a32904deeb186dc146af9b9a1ad05c798f37aa8
Vishnu <cs21b2037@iiitdm.ac.in>
2026-04-01 16:01:07 +0530

    Localization Initial Complete

--- models/localization.py
+++ models/localization.py
@@ -3,6 +3,9 @@
 
 import torch
 import torch.nn as nn
+from .layers import CustomDropout
+from .vgg11 import VGG11Encoder
+
 
 class VGG11Localizer(nn.Module):
     """VGG11-based localizer."""
@@ -15,7 +18,27 @@ class VGG11Localizer(nn.Module):
             in_channels: Number of input channels.
             dropout_p: Dropout probability for the localization head.
         """
-        pass
+        super().__init__()
+
+        # VGG11 Encoder
+        self.encoder = VGG11Encoder(in_channels)
+
+        # FC Layers
+        self.layer1 = nn.Sequential(
+            nn.Flatten(start_dim=1),
+            nn.Linear(in_features=25088, out_features=4096, bias=True),
+            nn.ReLU(inplace=True),
+            CustomDropout(dropout_p)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Linear(in_features=4096, out_features=1024, bias=True),
+            nn.ReLU(inplace=True),
+            CustomDropout(dropout_p)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(in_features=1024, out_features=4, bias=True),
+            nn.Sigmoid()
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass for localization model.
@@ -25,5 +48,10 @@ class VGG11Localizer(nn.Module):
         Returns:
             Bounding box coordinates [B, 4] in (x_center, y_center, width, height) format in original image pixel space(not normalized values).
         """
-        # TODO: Implement forward pass.
-        raise NotImplementedError("Implement VGG11Localizer.forward")
+        bottleneck = self.encoder(x)
+        x1 = self.layer1(bottleneck)
+        x2 = self.layer2(x1)
+        compressed_coords = self.layer3(x2)
+        coords = compressed_coords * 224.0
+
+        return coords
