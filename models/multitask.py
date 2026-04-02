7e6bcb94936e643c933a642a37d38cbb1a92680e
Vishnu <cs21b2037@iiitdm.ac.in>
2026-04-02 11:09:37 +0530

    Multi-Task Initial Complete

--- models/multitask.py
+++ models/multitask.py
@@ -3,11 +3,13 @@
 
 import torch
 import torch.nn as nn
+from .vgg11 import VGG11Encoder
+from .layers import CustomDropout
 
 class MultiTaskPerceptionModel(nn.Module):
     """Shared-backbone multi-task model."""
 
-    def __init__(self, num_breeds: int = 37, seg_classes: int = 3, in_channels: int = 3, classifier_path: str = "classifier.pth", localizer_path: str = "localizer.pth", unet_path: str = "unet.pth"):
+    def __init__(self, num_breeds: int = 37, seg_classes: int = 3, in_channels: int = 3, classifier_path: str = "classifier.pth", localizer_path: str = "localizer.pth", unet_path: str = "unet.pth", dropout_p: int = 0.5, encoder_backbone: str = "unet"):
         """
         Initialize the shared backbone/heads using these trained weights.
         Args:
@@ -17,8 +19,119 @@ class MultiTaskPerceptionModel(nn.Module):
             classifier_path: Path to trained classifier weights.
             localizer_path: Path to trained localizer weights.
             unet_path: Path to trained unet weights.
+            dropout_p: Dropout Probability to be followed in FC layers.
+            encoder_backbone: The name of architecture from which encoder is to be extracted from. One of ["unet", "classifier", "localizer"].
         """
-        pass
+        super().__init__()
+
+        # 1. Shared Backbone
+        self.encoder = VGG11Encoder(in_channels)
+
+        # 2. Rebuilding the Heads
+
+        # --- Classification Head ---
+        self.class_layer1 = nn.Sequential(
+            nn.Flatten(start_dim=1),
+            nn.Linear(in_features=25088, out_features=4096, bias=True),
+            nn.ReLU(inplace=True),
+            CustomDropout(dropout_p)
+        )
+        self.class_layer2 = nn.Sequential(
+            nn.Linear(in_features=4096, out_features=4096, bias=True),
+            nn.ReLU(inplace=True),
+            CustomDropout(dropout_p)
+        )
+        self.class_layer3 = nn.Linear(in_features=4096, out_features=num_breeds, bias=True)
+
+        # --- Localization Head ---
+        self.loc_layer1 = nn.Sequential(
+            nn.Flatten(start_dim=1),
+            nn.Linear(in_features=25088, out_features=4096, bias=True),
+            nn.ReLU(inplace=True),
+            CustomDropout(dropout_p)
+        )
+        self.loc_layer2 = nn.Sequential(
+            nn.Linear(in_features=4096, out_features=1024, bias=True),
+            nn.ReLU(inplace=True),
+            CustomDropout(dropout_p)
+        )
+        self.loc_layer3 = nn.Sequential(
+            nn.Linear(in_features=1024, out_features=4, bias=True),
+            nn.Sigmoid()
+        )
+
+        # --- Segmentation Head ---
+        self.up1 = nn.ConvTranspose2d(512, 512, kernel_size=2, stride=2)
+        self.dec1 = nn.Sequential(
+            nn.Conv2d(1024, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.up2 = nn.ConvTranspose2d(512, 512, kernel_size=2, stride=2)
+        self.dec2 = nn.Sequential(
+            nn.Conv2d(1024, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.up3 = nn.ConvTranspose2d(256, 256, kernel_size=2, stride=2)
+        self.dec3 = nn.Sequential(
+            nn.Conv2d(512, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        self.up4 = nn.ConvTranspose2d(128, 128, kernel_size=2, stride=2)
+        self.dec4 = nn.Sequential(            
+            nn.Conv2d(256, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.up5 = nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2)
+        self.dec5 = nn.Sequential(            
+            nn.Conv2d(128, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
+        self.outConv = nn.Conv2d(32, 3, kernel_size=1)
+
+        # 3. Loading the raw state dicts from the files
+        unet_state = torch.load(unet_path, map_location="cpu")
+        class_state = torch.load(classifier_path, map_location="cpu")
+        loc_state = torch.load(localizer_path, map_location="cpu")
+
+        # 4. Filtering encoder from states and renaming the layers in paths
+        class_head = {k.replace("layer", "class_layer"): v for k, v in class_state.items() if not k.startswith("encoder.")}
+        loc_head = {k.replace("layer", "loc_layer"): v for k, v in loc_state.items() if not k.startswith("encoder.")}
+        unet_head = {k: v for k, v in unet_state.items() if not k.startswith("encoder.")}
+
+        class_encoder = {k.replace("encoder.", ""): v for k, v in class_state.items() if k.startswith("encoder.")}
+        loc_encoder = {k.replace("encoder.", ""): v for k, v in loc_state.items() if k.startswith("encoder.")}
+        unet_encoder = {k.replace("encoder.", ""): v for k, v in unet_state.items() if k.startswith("encoder.")}
+
+        # 5. Loading only the encoder
+        if encoder_backbone == "unet":
+            self.encoder.load_state_dict(unet_encoder, strict=False)
+        elif encoder_backbone == "classifier":
+            self.encoder.load_state_dict(class_encoder, strict=False)
+        elif encoder_backbone == "localizer":
+            self.encoder.load_state_dict(loc_encoder, strict=False)
+        else:
+            raise ValueError("Invalid Backbone to extract Encoder from")
+        
+        # 6. Loading the heads
+        self.load_state_dict(class_head, strict=False)
+        self.load_state_dict(loc_head, strict=False)
+        self.load_state_dict(unet_head, strict=False)
+
+
 
     def forward(self, x: torch.Tensor):
         """Forward pass for multi-task model.
@@ -30,5 +143,40 @@ class MultiTaskPerceptionModel(nn.Module):
             - 'localization': [B, 4] bounding box tensor.
             - 'segmentation': [B, seg_classes, H, W] segmentation logits tensor
         """
-        # TODO: Implement forward pass.
-        raise NotImplementedError("Implement MultiTaskPerceptionModel.forward")
+        # 1. Shared Backbone
+        bottleneck, feature_maps = self.encoder(x, return_features=True)
+
+        # 2. Classification
+        x1 = self.class_layer1(bottleneck)
+        x2 = self.class_layer2(x1)
+        logits = self.class_layer3(x2)
+
+        # 3. Localization
+        x1 = self.loc_layer1(bottleneck)
+        x2 = self.loc_layer2(x1)
+        compressed_coords = self.loc_layer3(x2)
+        coords = compressed_coords * 224.0
+
+        # 4. Segmentation
+        u1 = self.up1(bottleneck)
+        cat1 = torch.cat([u1, feature_maps['block5']], dim=1)
+        x1 = self.dec1(cat1)
+        u2 = self.up2(x1)
+        cat2 = torch.cat([u2, feature_maps['block4']], dim=1)
+        x2 = self.dec2(cat2)
+        u3 = self.up3(x2)
+        cat3 = torch.cat([u3, feature_maps['block3']], dim=1)
+        x3 = self.dec3(cat3)
+        u4 = self.up4(x3)
+        cat4 = torch.cat([u4, feature_maps['block2']], dim=1)
+        x4 = self.dec4(cat4)
+        u5 = self.up5(x4)
+        cat5 = torch.cat([u5, feature_maps['block1']], dim=1)
+        x5 = self.dec5(cat5)
+        out = self.outConv(x5)
+
+        return {
+            "classification": logits,
+            "localization": coords,
+            "segmentation": out
+        }
